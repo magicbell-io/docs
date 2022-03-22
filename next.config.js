a774d7dda3a8a2ad467f8e1106a84ae93c600ede
Stephan Meijer <stephan.meijer@gmail.com>
2022-03-22 12:03:05 +0100

    chore: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 const withPlugins = require('next-compose-plugins');
 const redirects = require('./redirects.json');
 const withSvgr = require('next-svgr');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
-module.exports = withPlugins([withSvgr], {
+module.exports = withPlugins([[withBundleAnalyzer], withSvgr], {
   reactStrictMode: true,
   basePath: '/docs',
   webpack: (config, options) => {
